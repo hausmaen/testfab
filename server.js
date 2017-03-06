2d4c42271f2b02997b8a899b21f22b634100f4ed
hausmaen <github@hausammann.de>
2017-03-06 18:27:10 +0100

    Feierabend

--- server.js
+++ server.js
@@ -3,11 +3,30 @@ const app = express();
 var bodyParser  = require('body-parser');
 const jwt = require('jwt-simple');
 var passport	= require('passport');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var passport = require('passport');
+let config = require('./config/database'); // get db config file
+let User = require('./app/models/user'); // get the mongoose model
+let port = process.env.PORT || 8080;
 
+// get our request parameters
 app.use(bodyParser.urlencoded({extended: false}));
 
-alert("Hallo Welt2");
-
-
-
+app.use(bodyParser.json());
+ 
+// log to console
+app.use(morgan('dev'));
+ 
+// Use the passport package in our application
+app.use(passport.initialize());
+ 
+// demo Route (GET http://localhost:8080)
+app.get('/', function(req, res) {
+  res.send('Hello! The API is at http://localhost:' + port + '/api');
+});
+ 
+// Start the server
+app.listen(port);
+console.log('There will be dragons: http://localhost:' + port);
 
