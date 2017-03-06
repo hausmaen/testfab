ecd7f7ef3addd1eb5945be122adfa2cf1cfbfd74
hausmaen <github@hausammann.de>
2017-03-06 17:11:50 +0100

    test

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const app = express();
+var bodyParser  = require('body-parser');
+const jwt = require('jwt-simple');
+var passport	= require('passport');
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+
